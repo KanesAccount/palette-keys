7fc30488721ae9e5b4c19c38c4a2b3ebcbeaf8a0
KanesAccount <knwdevs@gmail.com>
2024-02-19 21:24:22 +0000

    chore: cleanup failed index attempt

--- demo/palette-keys-lwr/src/index.js
+++
@@ -1,6 +0,0 @@
-import { createElement } from 'lwc';
-import PaletteKeys from 'palette/keys';
-
-const app = createElement('palette-keys', { is: PaletteKeys });
-// eslint-disable-next-line @lwc/lwc/no-document-query
-document.querySelector('#main').appendChild(app);
